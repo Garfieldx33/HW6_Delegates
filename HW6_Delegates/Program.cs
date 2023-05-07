b28fcb833f87ea750a17f001f7eb75c3f953fbe5
DESKTOP-TJ96IS1\LAS <aleonchenko@intas-company.com>
2023-05-07 14:29:45 +0500

    Первая версия универсальной функции расширения

--- HW6_Delegates/Program.cs
+++ HW6_Delegates/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
 Console.WriteLine("Hello, World!");
+
+
+public static T GetMax(this IEnumerable e, Func<T, float> getParameter) where T : class
+{
+
+}
